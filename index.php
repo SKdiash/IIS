53c045b6521442f63e441666f0926a0c88f44792
NelaMm <macurova.nela@seznam.cz>
2017-11-22 18:54:55 +0100

    doplneni par kurzu

--- index.php
+++ index.php
@@ -12,6 +12,11 @@
     <h2>Vítáme Vás</h2>
     <p>na internetových stránkách Vzdělávacího střediska v Brně. Vzdělávací středisko je soukromou vzdělávací agenturou. V oblasti vzdělávání působí již od r. 2001, provozuje ho fyzická osoba Mgr. Jitka Okounová ve spolupráci s odbornými lektory a pedagogickými pracovníky.</p>
  <p>Nabízíme služby v oblasti vzdělávání, zajišťujeme profesní rozvoj, rozšiřování znalostí a dovedností pracovníků firem. Vzdělávací programy jsou určeny firmám i jednotlivcům. Připravujeme semináře, kvalifikační a rekvalifikační kurzy. Nabídka kurzů je orientována do oblasti informačních technologií.</p>
+
+<b>Kontakt</b><br>
+Adresa: Husova 12, Brno, 602 00<br>
+E-mail: info@kurzy.cz<br>
+Telefon: +420 522 522 522
 </div>
 
 <?php
