502acc100a20beac09730289350d50cd2d6bbab0
SKdiash <leramiao@gmail.com>
2017-11-28 14:35:49 +0100

    Pridani prijatych objednavek do ekonomiske stranky

--- economic.php
+++ economic.php
@@ -36,14 +36,15 @@
     $_SESSION["success_messages"] = '';
 
 
-echo '<table border="1" cellpadding="5" cellspacing="2" summary="Probíhající kurzy" id="Table2">
+echo '<table border="1" cellpadding="6" cellspacing="2" summary="Probíhající kurzy" id="Table2">
    <tbody>
       <tr>
-         <th colspan="5" scope="colgroup">Náklady a zisky</th>
+         <th colspan="6" scope="colgroup">Náklady a zisky</th>
       </tr>
       <tr>
          <th scope="col" abbr="Name">Jméno kurzu</th>
          <th scope="col" abbr="Tutor">Počet účastníků</th>   
+         <th scope="col">Datum</th>
          <th scope="col">Zisk</th>
          <th scope="col">Náklad</th>
          <th scope="col">Celkem</th>
@@ -68,14 +69,42 @@ echo '<table border="1" cellpadding="5" cellspacing="2" summary="Probíhající 
           echo '<tr>
                    <td scope="row">'.$course_all['name'].'</td>
                    <td>'.$course_on['number_logged'].'/'.$course_all['max_capacity'].'</td>
+                   <td>'.$course_all['date'].'</td>
                    <td>'.$zisk.'</td>
                    <td>'.$course_all['cost_course'].'</td>
                    <td>'.$celkem.'</td>';
                    
 
-          echo '</tr>';
+          echo '</tr>';  
+      }
+      
+    $listed_order = $mysqli->query("SELECT * FROM `order` WHERE accept = 1"); // zjistime kolik ted probiha kurzu
+    //echo $list_course->num_rows;
+    for($j = 1; $j <= ($listed_order->num_rows); $j++)
+    {  
+          
+          $order_accept = $listed_order->fetch_assoc();
+          $result = $mysqli->query("SELECT * FROM `course`, `order` WHERE order.id = '".$order_accept['id']."' AND course.id_course = order.id_course");
+          $order_all = $result->fetch_assoc();
+          
+          $zisk = $order_all['price_firm'];
+          $celkem = $zisk - $order_all['cost_course'];
+          $total = $total + $celkem;
+          echo '<tr>
+                   <td scope="row">'.$order_all['name'].'</td>
+                   <td>'.$order_all['max_capacity'].'/'.$order_all['max_capacity'].'</td>
+                   <td>'.$order_all['dates'].'</td>
+                   <td>'.$zisk.'</td>
+                   <td>'.$order_all['cost_course'].'</td>
+                   <td>'.$celkem.'</td>';
+                   
+
+          echo '</tr>';                    
     }
-    echo '<tr><td colspan="5" align="right">'.$total.'</td></tr>';
+          
+          
+        
+    echo '<tr><td colspan="6" align="right">'.$total.'</td></tr>';
     // pro uzivatele bych dala nejaky select from course where course.id = listed_course.id_course and listed_course.number_logged < course.max_cap
 
 ?>
